e0b3fd2851b8508b2b35ee20c0eafba86f596f4c
ankit <ankitbhallaa@gmail.com>
2025-07-16 14:52:12 +0530

    user schema added and roles file added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({}, { strict: false });
-
+ 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  phone: {
+    type: String,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  roles: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role'
+  }],
+}, { timestamps: true });
+ 
 module.exports = mongoose.model('User', userSchema);
